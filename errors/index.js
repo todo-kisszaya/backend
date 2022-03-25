acafe8fead6155259f85a1f94b77382c13b4cf2e
kisszaya <sveta.matvik.sveta@gmail.com>
2022-03-26 01:06:01 +0300

    feat(errors)

---
+++ errors/index.js
@@ -0,0 +1,8 @@
+const CustomAPIError = require('./custom-api')
+const UnauthenticatedError = require('./unauthenticated')
+const BadRequestError = require('./bad-request')
+const NotFoundError = require('./not-found')
+
+module.exports = {
+    CustomAPIError, NotFoundError, BadRequestError, UnauthenticatedError
+}
